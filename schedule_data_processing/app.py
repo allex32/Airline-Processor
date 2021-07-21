5b80a5e4388e199adc4fb468c42a275290c6f231
alex <matlashallex32@gmail.comm>
2021-07-21 23:33:53 +0300

    Minor changes. - Updated requirements.txt - Fixed output path for merge request - args list split to command/flight_number parameters

--- schedule_data_processing/app.py
+++ schedule_data_processing/app.py
@@ -1,11 +1,15 @@
+import sys
 from schedule_data_processing.commands.command_executor import CommandExecutor
 
+
 def main(args):
-    executor = CommandExecutor(args)
-    return executor.execute()
+    flight_numbers = None
+    if len(args) > 2:
+        flight_numbers = args[2]
+
+    executor = CommandExecutor(command=args[1], flight_numbers=flight_numbers)
+    return executor.execute().data
 
 
 if __name__ == "__main__":
-    print(main(list(["", "lookup", "ZG2361"])))
-    # print(main(sys.argv))
-
+    print(main(sys.argv))
